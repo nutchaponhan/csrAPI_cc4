5eab84614611de775be35d05ee924f70bc9d276f
nutchapon <nutchapon.han@gmail.com>
2019-10-18 09:15:38 +0700

    create database and connetion to pg

--- server.js
+++ server.js
@@ -6,10 +6,8 @@ const knex = require('knex')
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      user : 'postgres',
-      password : 'postgres',
-      database : 'codecamptest'
+        connectionString: process.env.DATABASE_URL,
+        ssl: true,
     }
   });
   
